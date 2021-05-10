5e00c8fa48f436cc488bc5daf88b0cdab8a0d70f
Marek Erben <erben.marek@gmail.com>
2021-05-10 12:53:25 +0200

    [#16070] LaOptika reservation form

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,4 +21,29 @@
     $(document).ready(function () {
         $('select').formSelect();
     });
+
+    // init Materialize CSS date & time pickers
+    $(document).ready(function () {
+        $('.datepicker').datepicker({
+            firstDay: 1, // monday
+            i18n: {
+                cancel: 'Zrušit',
+                clear: 'Smazat',
+                done: 'Hotovo',
+                months: ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Září', 'Říjen', 'Listopad', 'Prosinec'],
+                monthsShort: ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Září', 'Říjen', 'Listopad', 'Prosinec'],
+                weekdays: ['Neděle', 'Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota'],
+                weekdaysShort: ['Ne', 'Po', 'Út', 'St', 'Čt', 'Pá', 'So', 'Ne'],
+                weekdaysAbbrev: ['Ne', 'Po', 'Út', 'St', 'Čt', 'Pá', 'So', 'Ne']
+            }
+        });
+        $('.timepicker').timepicker({
+            twelveHour: false,
+            i18n: {
+                cancel: 'Zrušit',
+                clear: 'Smazat',
+                done: 'Hotovo'
+            }
+        });
+    });
 })(jQuery);
